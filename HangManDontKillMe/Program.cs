3341ab9410ed4d35de6759e4c3e0af69c7730247
Rand Farhan <rand.farhan@gmail.com>
2019-10-31 15:36:33 +0100

    Add project files.

---
+++ HangManDontKillMe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace HangManDontKillMe
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+
+
+            //secretWord 
+            string secretword = "SuperWoman";
+            List<string> letterGuessed = new List<string>();
+
+         
+
+
+            int live = 5; 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Find the missing Word!");
+
+            while (live >0 )
+            {
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
